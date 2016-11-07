dc78ab1452f11c200631ad4821ebb75cd5201ca8
Ayanle Hashi <ahashi2000@gmail.com>
2016-11-07 11:32:03 -0500

    Update RandomWordInfo.py

--- RandomWordInfo.py
+++ RandomWordInfo.py
@@ -15,18 +15,17 @@ class WordInfo:
         self.wordList = wordList
     
     def randWord(self):
+        #Prints a random word from the inputted word list
         return self.wordList[randint(0,len(self.wordList)-1)]
     
     def definition(self, word):
-        #This next section would randomly throw an IndexError, so I just made a
-        #try-except catch that just gets another random word in case one came 
-        #up.
         while 1:
             try:
                 #Get dictionary.com
                 link = "http://www.dictionary.com/browse/"+word
                 site = requests.get(link).text
                 soup = BeautifulSoup(site,"lxml")
+                #Find the divs with the definition and part of speech
                 divs = soup.findAll("div", {"class":"def-content"})
                 pos = soup.findAll("span", {"class":"dbox-pg"})
                 #Remove HTML formatting from extracted text
@@ -35,6 +34,7 @@ class WordInfo:
                 break
             except IndexError:
                 word = self.randWord()
+                continue
         return "\nWord: " + word + "\n\nPart of Speech: " + pos + "\n\nDefinition: " + definition
     
     def synonyms(self, word):
@@ -48,10 +48,17 @@ class WordInfo:
     def regexFind(self,pattern):
         r = re.compile(pattern)
         found = filter(r.match,self.wordList)
-        return "Found: " + ", ".join(found)
+        if len(found) > 100:
+            yn = raw_input("Found " + str(len(found)) + " matches, " + "print them all? (Y/N): ")
+            if yn in "Yy":
+                return "Found: " + ", ".join(found)
+            else:
+                return
+        else:
+            return "Found: " + ", ".join(found)
 
 w = WordInfo(l)
 word = raw_input("Enter a word: ")
 print w.definition(word)
 print w.synonyms(word)
-print w.regexFind("q[^u]")
+print w.regexFind("b")
